2804ee96205a8976393ec33dfd35b19cbe5b3f80
Jordan Alba <jordan008@live.ca>
2021-09-10 15:44:52 -0400

    Add project files.

---
+++ SimpleTipCalculator/SimpleTipCalculator/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+
+namespace SimpleTipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        double total = 0;
+        double totalTip = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void calcBill(object sender, System.EventArgs e)
+        {
+
+            totalTip = double.Parse(guests.Text) * double.Parse(tip.Text);
+            totTip.Text = totalTip.ToString();
+
+            total = double.Parse(bill.Text) + double.Parse(totTip.Text);
+            totBill.Text = total.ToString();
+
+        }
+
+        private void clear(object sender, System.EventArgs e)
+        {
+            bill.Text = "";
+            tip.Text = "";
+            guests.Text = "";
+
+        }
+    }
+}
